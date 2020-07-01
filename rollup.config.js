74fff8c89f535bcda69d2a7b1fd3fa23d22e278b
Abdul Ghofur <abdulghoin@gmail.com>
2020-07-01 10:03:20 +0700

    fix error when buildinng

--- rollup.config.js
+++ rollup.config.js
@@ -47,7 +47,7 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser() ghPages.publish('public', err => {
+		production && terser() && ghPages.publish('public', err => {
 			console.log("published", err)
 		})
 	],
